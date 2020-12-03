27868814308598a4828888742c497439b0aba5f0
insta <insta@DESKTOP-IAVPL5C>
2020-12-03 20:32:45 +0900

    Practice07

---
+++ src/com/javaex/ex02/Bird.java
@@ -0,0 +1,35 @@
+package com.javaex.ex02;
+
+public abstract class Bird {
+	// 필드
+	private String name;
+
+	// 생성자
+	public Bird() {
+	}
+
+	public Bird(String name) {
+		this.name = name;
+	}
+
+	// 메소드 g/s
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void sing() {
+
+	}
+
+	public void fly() {
+
+	}
+
+	public void showName() {
+	}
+
+}
